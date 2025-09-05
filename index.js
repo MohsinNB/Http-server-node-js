e043663b20ef454e6e5931e34ee75fa11c9e2aee
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-09-05 22:47:26 +0600

    delete all extra code and replace with express js

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const express = require("express");
-const App = express();
 
-function myHandler(req, res) {
-  if (req.url && req.url.includes("favicon")) return res.end();
-  // For local Time
-  const requestTime = new Date();
-  const localTime = new Date(requestTime.getTime() + 6 * 60 * 60 * 1000);
-  const gmt6String = localTime.toISOString().replace("T", " ").split(".")[0];
-  // local time end
-  const log = `request Time: { ${gmt6String} } with the {${req.method}} and thing is: {${req.url}}: new request in server\n`;
-  const Myurl = url.parse(req.url, true);
-  console.log(Myurl);
-  // console.log(Myurl);
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (Myurl.pathname) {
-      case "/":
-        res.end("Home page");
-        break;
-      case "/search":
-        const searchThing = Myurl.query.search_query;
-        res.end("Here are your result for " + searchThing);
-        break;
-      case "/about":
-        const userName = Myurl.query.Name;
-        res.end(`Hi, I am ${userName}`);
-        break;
-      case "/signup":
-        if (req.method === "GET") return res.end("This is a sign up form");
-        else if (req.method === "POST") {
-          // DB query
-          res.end("Form success");
-        }
-        break;
-      default:
-        res.end("404 not found");
-    }
-  });
-}
-const myServer = http.createServer(myHandler);
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello from Homepage");
+});
+app.get("/about", (req, res) => {
+  return res.send("This is for about section");
+});
+
+const myServer = http.createServer(app);
 
 myServer.listen(8000, () => console.log("server started"));
